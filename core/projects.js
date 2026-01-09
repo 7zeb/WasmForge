b3e0941790cb1dbc875d1d4cecd225c40f719779
7Zeb <66693249+7zeb@users.noreply.github.com>
2026-01-09 13:32:46 -0600

    Add aspectRatio to project object
    Added aspectRatio property to project object.

--- core/projects.js
+++ core/projects.js
@@ -2,5 +2,7 @@ export const project = {
   version: 1,
   title: "Untitled Project",
   media: [],      // { id, name, type }
-  timeline: []    // { mediaId, start, end }
+  timeline: [],    // { mediaId, start, end }
+  aspectRatio: "16:9" //the aspect ratio for the project (ie: 16:9, 9:16, 4:3, 1:1, 4:5, 21:1)
 };
+
