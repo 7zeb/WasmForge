dfb12e01a39c0e7ac81c16c0441ee5476fd40694
7Zeb <66693249+7zeb@users.noreply.github.com>
2026-01-13 13:17:32 -0600

    rewrite projects.js and make it better

--- core/projects.js
+++ core/projects.js
@@ -1,8 +1,15 @@
 export const project = {
   version: 1,
   title: "Untitled Project",
-  media: [],      // { id, name, type }
-  timeline: [],    // { mediaId, start, end }
-  aspectRatio: "16:9" //the aspect ratio for the project (ie: 16:9, 9:16, 4:3, 1:1, 4:5, 21:1)
-};
 
+  // Media files imported by the user
+  // { id, name, type }
+  media: [],
+
+  // Timeline clips
+  // { id, mediaId, x, width, start, end }
+  timeline: [],
+
+  // Project aspect ratio
+  aspectRatio: "16:9"
+};
